7343529d8d0dd616924894f5ed92a1c08a1b7c5e
Lucas Siqueira <lucasiqueira.tecnico@gmail.com>
2026-06-26 23:02:41 -0300

    Projeto sem alterações, gerado pelo Spring Initializr

---
+++ src/main/java/com/example/relembrando_conceitos/RelembrandoConceitosApplication.java
@@ -0,0 +1,13 @@
+package com.example.relembrando_conceitos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RelembrandoConceitosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RelembrandoConceitosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/relembrando_conceitos/RelembrandoConceitosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.relembrando_conceitos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RelembrandoConceitosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
